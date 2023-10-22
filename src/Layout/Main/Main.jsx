13475df081c6fd32ffce048fa36aa7f06b6d81d1
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-22 13:26:32 +0600

    some validation has fixed

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Home from "../../Pages/Home/Home";
+
+const Main = () => {
+    return (
+        <>
+            <Home/>
+        </>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <>
+            <div className="text-4xl text-center"> Hello baby </div>
+        </>
+    );
+};
+
+export default Home;
