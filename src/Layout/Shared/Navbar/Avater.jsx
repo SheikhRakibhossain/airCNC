f5c0e5a1a793203473753e8a0cbcfd5fe2e5f5d1
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-23 00:23:49 +0600

    user pic has added

--- src/Layout/Shared/Navbar/Avater.jsx
+++ src/Layout/Shared/Navbar/Avater.jsx
@@ -1,16 +1,18 @@
-import { createContext } from "react";
+import {  useContext } from "react";
 import avater from "../../../images/placeholder.jpg";
 import { AuthContext } from "../../../Provider/AuthProvider";
 const Avater = () => {
-  const { user } = createContext(AuthContext);
+  const { user } = useContext(AuthContext);
   return (
+   <>
     <img
-      src={user && user.photoURL ? user.photoURL : avater}
-      alt="profile image demo"
+      src={user && user.photoURL?user.photoURL:avater}
+      alt="profile image"
       className="rounded-full"
-      width={20}
+      width={30}
       height={30}
     />
+   </>
   );
 };
 
