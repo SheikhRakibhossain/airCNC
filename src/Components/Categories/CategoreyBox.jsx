f0f0d8fa90ea3db4f4d736e18cec377aa7379470
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-24 11:27:59 +0600

    query url has working without page navigate

--- src/Components/Categories/CategoreyBox.jsx
+++ src/Components/Categories/CategoreyBox.jsx
@@ -1,15 +1,40 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
 
 const CategoreyBox = ({ lable, icon: Icon }) => {
+  const navigate = useNavigate();
+  const [params, setParams] = useSearchParams();
+  const value = params.get("category");
+  console.log(value);
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+    const updateQuery = {
+      ...currentQuery,
+      category: lable,
+    };
 
-const [params, setParams] = useSearchParams();
-const value = params.get("category");
-console.log(value);
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updateQuery,
+      },
+      {
+        skipNull: true,
+      },
 
+      );
+      navigate(url)
+  };
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center p-2 gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer">
+      <div
+        onClick={handleClick}
+        className="flex flex-col justify-center items-center p-2 gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer"
+      >
         <Icon size="26px"></Icon>
         <div className="text-sm font-medium">{lable}</div>
       </div>
