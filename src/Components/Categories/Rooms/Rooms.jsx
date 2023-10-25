e807828b86de7a989ecacdb4f6e23b4c1c451061
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-25 23:45:43 +0600

    user page has created for test

--- src/Components/Categories/Rooms/Rooms.jsx
+++ src/Components/Categories/Rooms/Rooms.jsx
@@ -36,10 +36,10 @@ const Rooms = () => {
     <>
       <Container>
         {rooms && rooms.length > 0 ? (
-          <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
+          <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
             {rooms.map((room) => (
               <RoomCard key={room.image} room={room}></RoomCard>
-            ))}
+            ))} 
           </div>
         ) : (
           <div className="pt-12">
